2c26b0ffb1d2e21dfae4cc75152608cb05c1bab8
Kyle Steinfeld <ksteinfe@gmail.com>
2019-03-19 13:37:28 -0700

    ksteinfe.sh

--- ksteinfe.sh
+++ ksteinfe.sh
@@ -1,18 +1,17 @@
 ### Using labels only
 python train.py \
---name '190225_alamosquare' \
---dataroot '/media/ksteinfe/DATA/TEMP/alamosquare' \
+--name '190312_clarkkerr' \
+--dataroot '/media/ksteinfe/DATA/TEMP/clarkkerr' \
 --label_nc 0 \
 --no_instance \
 --loadSize 512 \
---save_epoch_freq 5 \
---load_pretrain /home/ksteinfe/GitHub/pix2pixHD/checkpoints/190222_eastcampus
-
+--save_epoch_freq 5 
 #--continue_train \
-#--which_epoch 60 \
+#--which_epoch 55 \
 
 # --lr 0.0008
 
+#--load_pretrain /home/ksteinfe/GitHub/pix2pixHD/checkpoints/190228_adamspoint
 
 #python test.py \
 #--name '190213_blijdorp' \
